8cd6082dc433a847c5d573e94c2bf076e2951792
Lucien Greathouse <me@lpghatguy.com>
2018-11-26 14:32:24 -0800

    Foundations of reader

--- rbxmx/src/deserializer.rs
+++ rbxmx/src/deserializer.rs
@@ -2,10 +2,18 @@ use std::io::Read;
 
 use rbx_tree::{RbxTree, RbxId};
 
-use xml::ParserConfig;
+use xml::{ParserConfig, EventReader, reader::{self, XmlEvent}};
 
 /// Indicates an error trying to parse an rbxmx or rbxlx document
 pub enum RbxmxParseError {
+    XmlError(reader::Error),
+    MalformedDocument,
+}
+
+impl From<reader::Error> for RbxmxParseError {
+    fn from(error: reader::Error) -> RbxmxParseError {
+        RbxmxParseError::XmlError(error)
+    }
 }
 
 /// Decodes source from the given buffer into the instance in the given tree.
@@ -13,9 +21,43 @@ pub enum RbxmxParseError {
 /// Roblox model files can contain multiple instances at the top level. This
 /// happens in the case of places as well as Studio users choosing multiple
 /// objects when saving a model file.
-pub fn decode<R: Read>(_tree: &mut RbxTree, _parent_id: RbxId, source: R) -> Result<(), RbxmxParseError> {
-    let _reader = ParserConfig::new()
+pub fn decode<R: Read>(tree: &mut RbxTree, parent_id: RbxId, source: R) -> Result<(), RbxmxParseError> {
+    let mut reader = ParserConfig::new()
         .create_reader(source);
+
+    deserialize_root(tree, parent_id, &mut reader)
+}
+
+#[allow(unused)]
+fn deserialize_root<R: Read>(tree: &mut RbxTree, parent_id: RbxId, reader: &mut EventReader<R>) -> Result<(), RbxmxParseError> {
+    match reader.next()? {
+        XmlEvent::StartDocument { .. } => {},
+        _ => return Err(RbxmxParseError::MalformedDocument),
+    }
+
+    match reader.next()? {
+        XmlEvent::StartElement { name, attributes, .. } => {
+            if name.local_name != "roblox" {
+                return Err(RbxmxParseError::MalformedDocument);
+            }
+
+            let mut found_version = false;
+            for attribute in &attributes {
+                if attribute.name.local_name == "version" {
+                    found_version = true;
+
+                    if attribute.value != "4" {
+                        return Err(RbxmxParseError::MalformedDocument);
+                    }
+                }
+            }
+
+            if !found_version {
+                return Err(RbxmxParseError::MalformedDocument);
+            }
+        },
+        _ => return Err(RbxmxParseError::MalformedDocument),
+    }
 
     unimplemented!()
 }
