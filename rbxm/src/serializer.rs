47f55be0e1b3eac3aaf1fe1c24af2dc141c1b49c
Lucien Greathouse <me@lpghatguy.com>
2018-11-27 18:30:31 -0800

    Add reference for file spec stuff

--- rbxm/src/serializer.rs
+++ rbxm/src/serializer.rs
@@ -2,6 +2,11 @@ use std::io::Write;
 
 use rbx_tree::{RbxTree, RbxId};
 
+// Format information taken from:
+// http://www.classy-studios.com/Downloads/RobloxFileSpec.pdf
+
+const FILE_HEADER = b"<roblox!\x89\xff\x0d\x0a\x1a\x0a\0\0";
+
 /// Serialize the instances denoted by `ids` from `tree` to XML.
 pub fn encode<W: Write>(tree: &RbxTree, ids: &[RbxId], output: W) {
     unimplemented!()
