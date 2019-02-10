0799bb5fc8b67ab5e8ed5441dd4cb12a78ba4012
Lucien Greathouse <me@lpghatguy.com>
2019-02-09 18:34:48 -0800

    Custom deserializer for RbxValue, take one

--- rbx_tree/src/value.rs
+++ rbx_tree/src/value.rs
@@ -1,6 +1,8 @@
-use serde_derive::{Serialize, Deserialize};
+use std::fmt;
 
-// TODO: Custom serialize/deserialize to encode literals more compactly in JSON.
+use serde_derive::{Serialize, Deserialize};
+use serde::{Deserialize, Deserializer};
+use serde::de::{self, Visitor, MapAccess};
 
 /// Represents a value that can be assigned to the properties of an instance.
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -82,3 +84,73 @@ pub enum RbxValue {
 /// Currently a stub.
 #[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
 pub struct PhysicalProperties;
+
+fn deserialize_value<'de, D>(deserializer: D) -> Result<RbxValue, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    struct RbxValueVisitor;
+
+    impl<'de> Visitor<'de> for RbxValueVisitor {
+        type Value = RbxValue;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("string or map")
+        }
+
+        fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            Ok(RbxValue::String {
+                value: value.to_owned(),
+            })
+        }
+
+        fn visit_map<M>(self, visitor: M) -> Result<Self::Value, M::Error>
+        where
+            M: MapAccess<'de>,
+        {
+            Deserialize::deserialize(de::value::MapAccessDeserializer::new(visitor))
+        }
+    }
+
+    deserializer.deserialize_any(RbxValueVisitor)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Deserialize)]
+    struct Wrapper(#[serde(deserialize_with = "deserialize_value")] RbxValue);
+
+    #[test]
+    fn explicit() {
+        let input = r#"
+            {
+                "Type": "String",
+                "Value": "Hello"
+            }
+        "#;
+
+        let value: Wrapper = serde_json::from_str(input).unwrap();
+
+        assert_eq!(value.0, RbxValue::String {
+            value: String::from("Hello"),
+        });
+    }
+
+    #[test]
+    fn implicit_string() {
+        let input = r#"
+            "Hello"
+        "#;
+
+        let value: Wrapper = serde_json::from_str(input).unwrap();
+
+        assert_eq!(value.0, RbxValue::String {
+            value: String::from("Hello"),
+        });
+    }
+}
