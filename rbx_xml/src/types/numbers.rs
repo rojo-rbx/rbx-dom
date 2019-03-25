cce410c9dbce2e1b01e8b4a9e97faa84479f51d3
Lucien Greathouse <me@lpghatguy.com>
2019-03-25 11:37:57 -0700

    Re-enable number tests

--- rbx_xml/src/types/numbers.rs
+++ rbx_xml/src/types/numbers.rs
@@ -9,7 +9,7 @@ use crate::{
 };
 
 macro_rules! number_type {
-    ($rbx_type: ident, $rust_type: ty, $xml_name: expr, $test_value: expr) => {
+    ($rbx_type: ident, $rust_type: ty, $xml_name: expr) => {
         pub struct $rbx_type;
 
         impl XmlType<$rust_type> for $rbx_type {
@@ -37,32 +37,99 @@ macro_rules! number_type {
                 })
             }
         }
-
-        // #[cfg(test)]
-        // #[test]
-        // fn round_trip() {
-        //     let _ = env_logger::try_init();
-
-        //     let test_input: $rust_type = $test_value;
-        //     let mut buffer = Vec::new();
-
-        //     let mut writer = XmlEventWriter::from_output(&mut buffer);
-        //     serialize(&mut writer, "foo", test_input).unwrap();
-
-        //     println!("{}", std::str::from_utf8(&buffer).unwrap());
-
-        //     let mut reader = EventIterator::from_source(buffer.as_slice());
-        //     reader.next().unwrap().unwrap(); // Eat StartDocument event
-        //     let value = deserialize(&mut reader).unwrap();
-
-        //     assert_eq!(value, RbxValue::$rbx_type {
-        //         value: test_input,
-        //     });
-        // }
     };
 }
 
-number_type!(Float32, f32, "float", 123456.0);
-number_type!(Float64, f64, "double", 123456.0);
-number_type!(Int32, i32, "int", -4654321);
-number_type!(Int64, i64, "int64", 281474976710656);
+number_type!(Float32, f32, "float");
+number_type!(Float64, f64, "double");
+number_type!(Int32, i32, "int");
+number_type!(Int64, i64, "int64");
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn round_trip_f32() {
+        let _ = env_logger::try_init();
+
+        let test_input = 123456.0;
+        let mut buffer = Vec::new();
+
+        let mut writer = XmlEventWriter::from_output(&mut buffer);
+        Float32::write_xml(&mut writer, "foo", &test_input).unwrap();
+
+        println!("{}", std::str::from_utf8(&buffer).unwrap());
+
+        let mut reader = EventIterator::from_source(buffer.as_slice());
+        reader.next().unwrap().unwrap(); // Eat StartDocument event
+        let value = Float32::read_xml(&mut reader).unwrap();
+
+        assert_eq!(value, RbxValue::Float32 {
+            value: test_input,
+        });
+    }
+
+    #[test]
+    fn round_trip_f64() {
+        let _ = env_logger::try_init();
+
+        let test_input = 123456.0;
+        let mut buffer = Vec::new();
+
+        let mut writer = XmlEventWriter::from_output(&mut buffer);
+        Float64::write_xml(&mut writer, "foo", &test_input).unwrap();
+
+        println!("{}", std::str::from_utf8(&buffer).unwrap());
+
+        let mut reader = EventIterator::from_source(buffer.as_slice());
+        reader.next().unwrap().unwrap(); // Eat StartDocument event
+        let value = Float64::read_xml(&mut reader).unwrap();
+
+        assert_eq!(value, RbxValue::Float64 {
+            value: test_input,
+        });
+    }
+
+    #[test]
+    fn round_trip_i32() {
+        let _ = env_logger::try_init();
+
+        let test_input = -4654321;
+        let mut buffer = Vec::new();
+
+        let mut writer = XmlEventWriter::from_output(&mut buffer);
+        Int32::write_xml(&mut writer, "foo", &test_input).unwrap();
+
+        println!("{}", std::str::from_utf8(&buffer).unwrap());
+
+        let mut reader = EventIterator::from_source(buffer.as_slice());
+        reader.next().unwrap().unwrap(); // Eat StartDocument event
+        let value = Int32::read_xml(&mut reader).unwrap();
+
+        assert_eq!(value, RbxValue::Int32 {
+            value: test_input,
+        });
+    }
+
+    #[test]
+    fn round_trip_i64() {
+        let _ = env_logger::try_init();
+
+        let test_input = 281474976710656;
+        let mut buffer = Vec::new();
+
+        let mut writer = XmlEventWriter::from_output(&mut buffer);
+        Int64::write_xml(&mut writer, "foo", &test_input).unwrap();
+
+        println!("{}", std::str::from_utf8(&buffer).unwrap());
+
+        let mut reader = EventIterator::from_source(buffer.as_slice());
+        reader.next().unwrap().unwrap(); // Eat StartDocument event
+        let value = Int64::read_xml(&mut reader).unwrap();
+
+        assert_eq!(value, RbxValue::Int64 {
+            value: test_input,
+        });
+    }
+}
