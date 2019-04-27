72f396f20a1685a52e04cb58ca64590f1bca7cfc
Lucien Greathouse <me@lpghatguy.com>
2019-04-26 18:44:59 -0700

    Document top-level methods

--- rbx_reflection/src/reflection_database.rs
+++ rbx_reflection/src/reflection_database.rs
@@ -16,21 +16,27 @@ lazy_static! {
     static ref ENUMS: HashMap<Cow<'static, str>, RbxEnumDescriptor> = self::enums::generate_enums();
 }
 
+/// Returns the class descriptor with the given name from the reflection
+/// database, if it exists.
 #[inline]
 pub fn get_class_descriptor(name: &str) -> Option<&'static RbxClassDescriptor> {
     CLASSES.get(name)
 }
 
+/// Returns an iterator over all class descriptors in the reflection database.
 #[inline]
 pub fn iter_class_descriptors() -> impl Iterator<Item = (&'static str, &'static RbxClassDescriptor)> {
     CLASSES.iter().map(|(key, value)| (key.as_ref(), value))
 }
 
+/// Returns the enum descriptor with the given name from the reflection
+/// database, if it exists.
 #[inline]
 pub fn get_enum_descriptor(name: &str) -> Option<&'static RbxEnumDescriptor> {
     ENUMS.get(name)
 }
 
+/// Returns an itertaor over all enum descriptors in the reflection database.
 #[inline]
 pub fn iter_enum_descriptors() -> impl Iterator<Item = (&'static str, &'static RbxEnumDescriptor)> {
     ENUMS.iter().map(|(key, value)| (key.as_ref(), value))
