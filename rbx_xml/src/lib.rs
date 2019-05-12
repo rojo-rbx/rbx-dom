1e5130e47ea94e03dcb7c879fce1e8715a53bef2
Lucien Greathouse <me@lpghatguy.com>
2019-05-11 18:36:32 -0700

    Improve main crate docs

--- rbx_xml/src/lib.rs
+++ rbx_xml/src/lib.rs
@@ -6,6 +6,40 @@
 //! This crate implements most of the format and is driven by an up-to-date
 //! reflection database.
 //!
+//! To decode a place, use a method like `from_reader` if you're reading from a
+//! file, or `from_str` if you already have a string. These methods also have
+//! variants that pass in default configuration.
+//!
+//! ```rust,no_run
+//! # // FIXME: This test overflows its stack only as a doctest. :/
+//!
+//! use rbx_dom_weak::RbxValue;
+//!
+//! let model_file = r#"
+//! <roblox version="4">
+//!     <Item class="NumberValue" referent="RBX3B3D9D3DB43D4E6793B190B081E0A886">
+//!         <Properties>
+//!             <string name="Name">My NumberValue</string>
+//!             <double name="Value">12345</double>
+//!         </Properties>
+//!     </Item>
+//! </roblox>
+//! "#;
+//!
+//! let tree = rbx_xml::from_str_default(model_file)
+//!     .expect("Couldn't decode model file");
+//!
+//! let data_model = tree.get_instance(tree.get_root_id()).unwrap();
+//! let number_value_id = data_model.get_children_ids()[0];
+//!
+//! let number_value = tree.get_instance(number_value_id).unwrap();
+//!
+//! assert_eq!(
+//!     number_value.properties.get("Value"),
+//!     Some(&RbxValue::Float64 { value: 12345.0 }),
+//! );
+//! ```
+//!
 //! [rbx_dom_weak]: https://crates.io/crates/rbx_dom_weak
 
 #![deny(missing_docs)]
