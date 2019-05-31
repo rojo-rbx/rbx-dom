01982fda8bb70195fe7dd9e2d1eaa85b20c35b01
Lucien Greathouse <me@lpghatguy.com>
2019-05-31 15:11:21 -0700

    Add TestEZ-based tests

--- rbx_dom_lua/test.server.lua
+++ rbx_dom_lua/test.server.lua
@@ -1,21 +1,7 @@
-local Workspace = game:GetService("Workspace")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local RbxDom = require(ReplicatedStorage.RbxDom)
+local LIB_ROOT = ReplicatedStorage.RbxDom
 
-local CanonicalInstance = RbxDom.CanonicalInstance
-local CanonicalProperty = RbxDom.CanonicalProperty
-local PropertySelection = RbxDom.PropertySelection
+local TestEZ = require(ReplicatedStorage.TestEZ)
 
-local baseplate = Workspace.Baseplate
-local baseplateProperties = CanonicalInstance.readInstance(Workspace.Baseplate, PropertySelection.MinimalSerializable)
-
-for key, value in pairs(baseplateProperties) do
-	print(key, value)
-end
-
-local newBaseplate = Instance.new("Part")
-CanonicalInstance.writeInstance(newBaseplate, baseplateProperties)
-
-baseplate:Destroy()
-newBaseplate.Parent = Workspace
+TestEZ.TestBootstrap:run({LIB_ROOT})
