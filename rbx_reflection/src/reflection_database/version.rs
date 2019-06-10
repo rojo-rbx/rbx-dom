bb2f6f1f80e1d49a10586e7f9a1779a1845ae249
Lucien Greathouse <me@lpghatguy.com>
2019-06-10 16:54:28 -0700

    rbx_reflection: Add generated clippy annotation

--- rbx_reflection/src/reflection_database/version.rs
+++ rbx_reflection/src/reflection_database/version.rs
@@ -1,4 +1,5 @@
 // This file is automatically @generated.
+#![allow(clippy::unreadable_literal)]
 pub const RBX_VERSION_MAJOR: u32 = 0;
 pub const RBX_VERSION_MINOR: u32 = 388;
 pub const RBX_VERSION_PATCH: u32 = 0;
