f7ef8859087d4c4c5670fb622551d85ac6afccc1
Lucien Greathouse <me@lpghatguy.com>
2019-07-11 11:29:55 -0700

    Add test for team with no reflection info

--- rbx_xml/tests/brick_color.rs
+++ rbx_xml/tests/brick_color.rs
@@ -24,6 +24,7 @@ fn spawn_location() {
     }));
 }
 
+/// Asserts that 'Team.TeamColor' deserializes correctly as a BrickColor.
 #[test]
 fn team() {
     let _ = env_logger::try_init();
@@ -42,6 +43,14 @@ fn team() {
     }));
 }
 
+/// Roblox has name collisions for BrickColors!
+///
+/// This test catches one of them, Lilac and Lilac, but 'Gold', 'Deep
+/// orange', and 'Rust' (haha) also collide.
+///
+/// rbx_dom_weak reserves the enum variant for the version that Roblox picks
+/// when `BrickColor.new("Lilac")` is run. The other variant has '2'
+/// appended.
 #[test]
 fn team_lilac() {
     let _ = env_logger::try_init();
@@ -56,18 +65,33 @@ fn team_lilac() {
     assert_eq!(team.name, "Team");
     assert_eq!(team.class_name, "Team");
 
-    // Roblox has name collisions for BrickColors!
-    //
-    // This test catches one of them, Lilac and Lilac, but 'Gold', 'Deep
-    // orange', and 'Rust' (haha) also collide.
-    //
-    // rbx_dom_weak reserves the enum variant for the version that Roblox picks
-    // when `BrickColor.new("Lilac")` is run. The other variant has '2'
-    // appended.
     assert_ne!(team.properties.get("TeamColor"), Some(&RbxValue::BrickColor {
         value: BrickColor::Lilac, // This Lilac has a value of 321
     }));
     assert_eq!(team.properties.get("TeamColor"), Some(&RbxValue::BrickColor {
         value: BrickColor::Lilac2, // This Lilac has a value of 219
+    }));
+}
+
+/// When BrickColor values are deserialized without reflection information, they
+/// should turn into regular Int32 values.
+#[test]
+fn team_no_reflection() {
+    let _ = env_logger::try_init();
+
+    let options = rbx_xml::DecodeOptions::new()
+        .property_behavior(rbx_xml::DecodePropertyBehavior::NoReflection);
+
+    let tree = rbx_xml::from_reader(TEAM, options).unwrap();
+    let root_id = tree.get_root_id();
+
+    let root_instance = tree.get_instance(root_id).unwrap();
+    let team_id = root_instance.get_children_ids()[0];
+    let team = tree.get_instance(team_id).unwrap();
+
+    assert_eq!(team.name, "Team");
+    assert_eq!(team.class_name, "Team");
+    assert_eq!(team.properties.get("TeamColor"), Some(&RbxValue::Int32 {
+        value: 1020,
     }));
 }
