c50932aa0454fa15ba4d2778f85da0433ac59636
Lucien Greathouse <me@lpghatguy.com>
2019-08-29 18:31:08 -0700

    Fix failing cargo fmt call on Linux only?

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -6,6 +6,6 @@ cargo test --all --verbose
 
 # Skip formatting generate_reflection because of
 # https://github.com/rust-lang/rustfmt/issues/3688
-cargo fmt --package rbx_binary rbx_dom_weak rbx_reflection rbx_xml -- --check
+cargo fmt --package rbx_binary --package rbx_dom_weak --package rbx_reflection --package rbx_xml -- --check
 
 cargo clippy
