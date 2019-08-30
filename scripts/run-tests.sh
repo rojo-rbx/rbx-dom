acf211094933a283d29dac897e09d341da1cbff6
Lucien Greathouse <me@lpghatguy.com>
2019-08-29 18:16:26 -0700

    Fix Travis build scripts

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -6,6 +6,6 @@ cargo test --all --locked --verbose
 
 # Skip formatting generate_reflection because of
 # https://github.com/rust-lang/rustfmt/issues/3688
-cargo fmt -- --check --package rbx_binary rbx_dom_weak rbx_reflection rbx_xml
+cargo fmt --package rbx_binary rbx_dom_weak rbx_reflection rbx_xml -- --check
 
 cargo clippy
