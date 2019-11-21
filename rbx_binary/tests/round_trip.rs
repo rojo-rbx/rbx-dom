0a3d08a62a62d93e59992bc0fcad4234f2202e6a
Lucien Greathouse <me@lpghatguy.com>
2019-11-20 18:02:23 -0800

    Clean up round trip test and add shim for replacing binary serializer impl

--- rbx_binary/tests/round_trip.rs
+++ rbx_binary/tests/round_trip.rs
@@ -1,9 +1,9 @@
-use std::{collections::HashMap, fs, io::Cursor, path::Path};
-
-use log::trace;
+use std::{collections::HashMap, fs, path::Path};
 
 use rbx_dom_test::TreeViewer;
 use rbx_dom_weak::{RbxInstanceProperties, RbxTree};
+
+use rbx_binary::encode;
 
 #[test]
 fn just_folder() {
@@ -38,7 +38,7 @@ fn test_round_trip(test_name: &str) {
     let root_instance = initial_tree.get_instance(root_id).unwrap();
     let root_children = root_instance.get_children_ids();
     let mut roundtrip_contents = Vec::new();
-    rbx_binary::encode(&initial_tree, root_children, &mut roundtrip_contents).unwrap();
+    encode(&initial_tree, root_children, &mut roundtrip_contents).unwrap();
 
     let mut roundtrip_tree = new_test_tree();
 
