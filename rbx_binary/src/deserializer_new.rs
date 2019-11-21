fc0ee65610272279295dd5cf76814ef491c30d68
Lucien Greathouse <me@lpghatguy.com>
2019-11-21 11:39:50 -0800

    Skeleton more of new deserializer

--- rbx_binary/src/deserializer_new.rs
+++ rbx_binary/src/deserializer_new.rs
@@ -3,7 +3,13 @@ use std::{
     io::{self, Read},
 };
 
+use byteorder::{LittleEndian, ReadBytesExt};
 use rbx_dom_weak::{RbxId, RbxInstanceProperties, RbxTree};
+
+use crate::{
+    chunk::Chunk,
+    core::{FILE_MAGIC_HEADER, FILE_SIGNATURE, FILE_VERSION},
+};
 
 /// A compatibility shim to expose the new deserializer with the API of the old
 /// deserializer.
@@ -24,7 +30,7 @@ pub fn decode_compat<R: Read>(
 }
 
 pub fn decode<R: Read>(mut input: R) -> io::Result<RbxTree> {
-    let deserializer = BinaryDeserializer::new(input);
+    let deserializer = BinaryDeserializer::new(input)?;
 
     Ok(deserializer.finish())
 }
@@ -36,17 +42,95 @@ struct BinaryDeserializer<R> {
     /// The tree that instances should be written into. Eventually returned to
     /// the user.
     tree: RbxTree,
+
+    /// The data about the file contained in the header.
+    header: FileHeader,
+
+    /// Currently unused. The metadata contained in the file, which affects how
+    /// some constructs are interpreted by Roblox.
+    metadata: HashMap<String, String>,
+}
+
+struct FileHeader {
+    num_instance_types: u32,
+    num_instances: u32,
 }
 
 impl<R: Read> BinaryDeserializer<R> {
-    fn new(input: R) -> Self {
+    fn new(mut input: R) -> io::Result<Self> {
         let tree = make_temp_output_tree();
 
-        BinaryDeserializer { input, tree }
+        let header = FileHeader::decode(&mut input)?;
+
+        Ok(BinaryDeserializer {
+            input,
+            tree,
+            header,
+            metadata: HashMap::new(),
+        })
+    }
+
+    fn decode_meta_chunk(&mut self, chunk: Chunk) -> io::Result<()> {
+        Ok(())
+    }
+
+    fn decode_inst_chunk(&mut self, chunk: Chunk) -> io::Result<()> {
+        Ok(())
+    }
+
+    fn decode_prop_chunk(&mut self, chunk: Chunk) -> io::Result<()> {
+        Ok(())
+    }
+
+    fn decode_prnt_chunk(&mut self, chunk: Chunk) -> io::Result<()> {
+        Ok(())
+    }
+
+    fn decode_end_chunk(&mut self, chunk: Chunk) -> io::Result<()> {
+        Ok(())
     }
 
     fn finish(self) -> RbxTree {
         self.tree
+    }
+}
+
+impl FileHeader {
+    fn decode<R: Read>(mut source: R) -> io::Result<Self> {
+        let mut magic_header = [0; 8];
+        source.read_exact(&mut magic_header)?;
+
+        if &magic_header != FILE_MAGIC_HEADER {
+            panic!("Mismatched magic header");
+        }
+
+        let mut signature = [0; 6];
+        source.read_exact(&mut signature)?;
+
+        if &signature != FILE_SIGNATURE {
+            panic!("Mismatched file signature");
+        }
+
+        let version = source.read_u16::<LittleEndian>()?;
+
+        if version != FILE_VERSION {
+            panic!("Unknown file version");
+        }
+
+        let num_instance_types = source.read_u32::<LittleEndian>()?;
+        let num_instances = source.read_u32::<LittleEndian>()?;
+
+        let mut reserved = [0; 8];
+        source.read_exact(&mut reserved)?;
+
+        if reserved != [0; 8] {
+            panic!("Invalid reserved bytes");
+        }
+
+        Ok(Self {
+            num_instance_types,
+            num_instances,
+        })
     }
 }
 
