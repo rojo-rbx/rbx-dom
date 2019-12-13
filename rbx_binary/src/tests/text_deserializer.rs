52d9bd4bd0ad3253b6181d93d75e3cd654948734
Lucien Greathouse <me@lpghatguy.com>
2019-12-13 10:49:59 -0800

    Add nested folders test

--- rbx_binary/src/tests/text_deserializer.rs
+++ rbx_binary/src/tests/text_deserializer.rs
@@ -2,15 +2,22 @@ use crate::debug_deserializer::DecodedModel;
 
 static FOLDER: &[u8] = include_bytes!("../../test-files/from-studio/default-inserted-folder.rbxm");
 static PART: &[u8] = include_bytes!("../../test-files/from-studio/default-inserted-part.rbxm");
+static NESTED_FOLDERS: &[u8] = include_bytes!("../../test-files/from-studio/nested-folders.rbxm");
 
 #[test]
-fn folder_from_studio() {
+fn default_inserted_folder() {
     let decoded = DecodedModel::from_reader(FOLDER);
     insta::assert_yaml_snapshot!(decoded);
 }
 
 #[test]
-fn part_from_studio() {
+fn default_inserted_part() {
     let decoded = DecodedModel::from_reader(PART);
+    insta::assert_yaml_snapshot!(decoded);
+}
+
+#[test]
+fn nested_folders() {
+    let decoded = DecodedModel::from_reader(NESTED_FOLDERS);
     insta::assert_yaml_snapshot!(decoded);
 }
