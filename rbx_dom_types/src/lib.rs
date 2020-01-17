f1afb0b91dc242eb1b3f518f2afdb62aee1c65f9
Lucien Greathouse <me@lpghatguy.com>
2020-01-16 17:49:39 -0800

    Add baseline for rbx_dom_types crate

---
+++ rbx_dom_types/src/lib.rs
@@ -0,0 +1,100 @@
+pub struct Vector2 {
+    pub x: f32,
+    pub y: f32,
+}
+
+pub struct Vector2int16 {
+    pub x: i16,
+    pub y: i16,
+}
+
+pub struct Vector3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+pub struct Vector3int16 {
+    pub x: i16,
+    pub y: i16,
+    pub z: i16,
+}
+
+pub struct CFrame {
+    pub position: Vector3,
+    pub orientation: [f32; 9],
+}
+
+pub struct Color3 {
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+}
+
+pub struct Color3uint8 {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+pub struct Ray {
+    pub origin: Vector3,
+    pub direction: Vector3,
+}
+
+pub struct Region3 {
+    pub min: Vector3,
+    pub max: Vector3,
+}
+
+pub struct Region3int16 {
+    pub min: Vector3int16,
+    pub max: Vector3int16,
+}
+
+pub struct Rect {
+    pub min: Vector2,
+    pub max: Vector2,
+}
+
+pub struct UDim {
+    pub scale: f32,
+    pub offset: i32,
+}
+
+pub struct UDim2 {
+    pub x: UDim,
+    pub y: UDim,
+}
+
+pub struct PhysicalProperties {
+    pub density: f32,
+    pub friction: f32,
+    pub elasticity: f32,
+    pub friction_weight: f32,
+    pub elasticity_weight: f32,
+}
+
+pub struct NumberRange {
+    pub min: f32,
+    pub max: f32,
+}
+
+pub struct ColorSequence {
+    pub keypoints: Vec<ColorSequenceKeypoint>,
+}
+
+pub struct ColorSequenceKeypoint {
+    pub time: f32,
+    pub color: Color3,
+}
+
+pub struct NumberSequence {
+    pub keypoints: Vec<NumberSequenceKeypoint>,
+}
+
+pub struct NumberSequenceKeypoint {
+    pub time: f32,
+    pub value: f32,
+    pub envelope: f32,
+}
