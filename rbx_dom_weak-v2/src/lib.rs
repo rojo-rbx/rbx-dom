13a806ae17a13e66d3d6442fb597d316019e3244
Lucien Greathouse <me@lpghatguy.com>
2020-02-05 15:24:38 -0800

    Fix tests and docs

--- rbx_dom_weak-v2/src/lib.rs
+++ rbx_dom_weak-v2/src/lib.rs
@@ -3,68 +3,48 @@
 //! instances by ID in constant time.
 //!
 //! Constructing a new tree of instances is accomplished by first creating an
-//! [`RbxInstanceProperties`] object that describes the root instance of the
-//! tree, and then wrapping it with an [`WeakDom`]:
+//! [`InstanceBuilder`] object that describes a tree of instances and then
+//! wrapping it with an [`WeakDom`]:
 //!
 //! ```
-//! use std::collections::HashMap;
-//! use rbx_dom_weak::{RbxInstanceProperties, WeakDom};
+//! use rbx_dom_weak2::{InstanceBuilder, WeakDom};
 //!
-//! let props = RbxInstanceProperties {
-//!     name: "My Cool Game".to_owned(),
-//!     class_name: "DataModel".to_owned(),
-//!     properties: HashMap::new(),
-//! };
+//! let dm = InstanceBuilder::new("DataModel");
 //!
-//! let mut tree = WeakDom::new(props);
-//! println!("ID of instance we just inserted is {:?}", tree.get_root_id());
+//! let mut dom = WeakDom::new(dm);
+//!
+//! println!("ID of DOM root is {:?}", dom.root_ref());
 //! ```
 //!
-//! Note that the [maplit] crate is incredibly useful for defining properties
-//! inline.
-//!
-//! Once we have a tree, we can use [`WeakDom::insert_instance`] and
-//! [`WeakDom::get_instance`] to add instances to the tree and retrieve them.
+//! Once we have a tree, we can use [`WeakDom::insert`] and
+//! [`WeakDom::get_by_ref`] to add instances to the tree and retrieve them.
 //!
 //! ```
-//! # use std::collections::HashMap;
-//! # use rbx_dom_weak::{RbxInstanceProperties, WeakDom};
-//! use rbx_dom_weak::types::Variant;
-//! use maplit::hashmap;
-//! #
-//! # let props = RbxInstanceProperties {
-//! #     name: "My Cool Game".to_owned(),
-//! #     class_name: "DataModel".to_owned(),
-//! #     properties: HashMap::new(),
-//! # };
-//! #
-//! # let mut tree = WeakDom::new(props);
-//! #
-//! let http_service = RbxInstanceProperties {
-//!     name: "HttpService".to_owned(),
-//!     class_name: "HttpService".to_owned(),
-//!     properties: hashmap! {
-//          // Properties are represented via the Variant enum
-//!         "HttpEnabled".to_owned() => true.into(),
-//!     },
-//! };
+//! use rbx_dom_weak2::{InstanceBuilder, WeakDom};
 //!
-//! let datamodel_id = tree.get_root_id();
-//! let http_service_id = tree.insert_instance(http_service, datamodel_id);
+//! let mut dom = WeakDom::new(InstanceBuilder::new("DataModel"));
+//!
+//! // We can define properties using any type that can be converted to an
+//! // rbx_dom_weak::types::Variant.
+//! let http_service = InstanceBuilder::new("HttpService")
+//!     .with_property("HttpEnabled", true);
+//!
+//! let http_service_id = dom.insert(dom.root_ref(), http_service);
 //!
 //! println!("HttpService has ID {:?}", http_service_id);
 //! ```
 //!
 //! To change properties on an instance that's already present in the tree, use
-//! [`WeakDom::get_instance_mut`]. Note that it isn't possible to add or remove
-//! children through this method, use [`WeakDom::insert_instance`] instead.
+//! [`WeakDom::get_by_ref_mut`]. Note that it isn't possible to add or remove
+//! children through this method, use [`WeakDom::insert`] and
+//! [`WeakDom::destroy`] instead.
 //!
 //! [`WeakDom`]: struct.WeakDom.html
-//! [`WeakDom::insert_instance`]: struct.WeakDom.html#method.insert_instance
-//! [`WeakDom::get_instance`]: struct.WeakDom.html#method.get_instance
-//! [`WeakDom::get_instance_mut`]: struct.WeakDom.html#method.get_instance_mut
-//! [`RbxInstanceProperties`]: struct.RbxInstanceProperties.html
-//! [maplit]: https://crates.io/crates/maplit
+//! [`WeakDom::insert`]: struct.WeakDom.html#method.insert
+//! [`WeakDom::destroy`]: struct.WeakDom.html#method.destroy
+//! [`WeakDom::get_by_ref`]: struct.WeakDom.html#method.get_by_ref
+//! [`WeakDom::get_by_ref_mut`]: struct.WeakDom.html#method.get_by_ref_mut
+//! [`InstanceBuilder`]: struct.InstanceBuilder.html
 
 mod dom;
 mod instance;
