8ebdfa2d1649132f80e15639791c5275ad4c1bf0
Lucien Greathouse <me@lpghatguy.com>
2020-02-06 10:56:37 -0800

    Implement rbx_reflection_database using embedded MessagePack

--- rbx_reflection_database/src/lib.rs
+++ rbx_reflection_database/src/lib.rs
@@ -1,1 +1,23 @@
+use rbx_reflection::ReflectionDatabase;
 
+static ENCODED_DATABASE: &[u8] = include_bytes!("../database.msgpack");
+
+lazy_static::lazy_static! {
+    static ref DATABASE: ReflectionDatabase<'static> = {
+        rmp_serde::decode::from_slice(ENCODED_DATABASE).unwrap()
+    };
+}
+
+pub fn get() -> &'static ReflectionDatabase<'static> {
+    &DATABASE
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn smoke_test() {
+        let _database = get();
+    }
+}
