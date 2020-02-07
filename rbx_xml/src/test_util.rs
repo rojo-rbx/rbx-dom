99e6bb9983f1a41a454a1755d63267cb23de570b
Lucien Greathouse <me@lpghatguy.com>
2020-02-06 18:01:48 -0800

    Port test_util to new traits

--- rbx_xml/src/test_util.rs
+++ rbx_xml/src/test_util.rs
@@ -1,40 +1,38 @@
-use rbx_dom_weak::RbxValue;
+use std::fmt::Debug;
 
 use crate::{core::XmlType, deserializer_core::XmlEventReader, serializer_core::XmlEventWriter};
 
-pub fn test_xml_round_trip<Xml, InnerType>(test_value: &InnerType, wrapped_value: RbxValue)
+pub fn test_xml_round_trip<T>(test_value: &T)
 where
-    Xml: XmlType<InnerType>,
-    InnerType: ?Sized,
+    T: XmlType + PartialEq + Debug,
 {
     let _ = env_logger::try_init();
 
     let mut buffer = Vec::new();
     let mut writer = XmlEventWriter::from_output(&mut buffer);
 
-    Xml::write_xml(&mut writer, "foo", test_value).unwrap();
+    test_value.write_xml(&mut writer, "foo").unwrap();
 
     println!("{}", std::str::from_utf8(&buffer).unwrap());
 
     let mut reader = XmlEventReader::from_source(buffer.as_slice());
     reader.next().unwrap().unwrap(); // Eat StartDocument event
 
-    let value = Xml::read_xml(&mut reader).unwrap();
+    let value = T::read_xml(&mut reader).unwrap();
 
-    assert_eq!(value, wrapped_value);
+    assert_eq!(&value, test_value);
 }
 
-pub fn test_xml_serialize<Xml, InnerType>(expected_source: &str, test_value: &InnerType)
+pub fn test_xml_serialize<T>(expected_source: &str, test_value: &T)
 where
-    Xml: XmlType<InnerType>,
-    InnerType: ?Sized,
+    T: XmlType + PartialEq + Debug,
 {
     let _ = env_logger::try_init();
 
     let mut buffer = Vec::new();
     let mut writer = XmlEventWriter::from_output(&mut buffer);
 
-    Xml::write_xml(&mut writer, "foo", test_value).unwrap();
+    test_value.write_xml(&mut writer, "foo").unwrap();
 
     let mut expected_events = XmlEventReader::from_source(expected_source.as_bytes());
     let mut actual_events = XmlEventReader::from_source(buffer.as_slice());
@@ -68,16 +66,15 @@ where
     }
 }
 
-pub fn test_xml_deserialize<Xml, InnerType>(source: &str, expected_value: RbxValue)
+pub fn test_xml_deserialize<T>(source: &str, expected_value: &T)
 where
-    Xml: XmlType<InnerType>,
-    InnerType: ?Sized,
+    T: XmlType + PartialEq + Debug,
 {
     let _ = env_logger::try_init();
 
     let mut reader = XmlEventReader::from_source(source.as_bytes());
     reader.next().unwrap().unwrap(); // Eat StartDocument event
-    let value = Xml::read_xml(&mut reader).unwrap();
+    let value = T::read_xml(&mut reader).unwrap();
 
-    assert_eq!(value, expected_value);
+    assert_eq!(&value, expected_value);
 }
