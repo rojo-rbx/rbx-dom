4964178e2aa9afe0998914e6c27bb8141945d9da
Lucien Greathouse <me@lpghatguy.com>
2020-04-20 23:20:56 -0700

    Tests

---
+++ rbx_xml/tests/test-files.rs
@@ -0,0 +1,35 @@
+use std::{fs, path::Path};
+
+use rbx_dom_weak::DomViewer;
+
+macro_rules! test_models {
+    ( $( $test_name: ident : $file_name: expr,)* ) => {
+        $(
+            #[test]
+            fn $test_name() {
+                let _ = env_logger::try_init();
+
+                let mut path = Path::new("../test-files/models").join($file_name);
+                path.push("xml.rbxmx");
+
+                let contents = fs::read_to_string(path).unwrap();
+                let dom = rbx_xml::from_str_default(&contents).unwrap();
+
+                let mut viewer = DomViewer::new();
+
+                insta::assert_yaml_snapshot!(viewer.view_children(&dom));
+            }
+        )*
+    };
+}
+
+test_models! {
+    default_inserted_folder: "default-inserted-folder",
+    default_inserted_part: "default-inserted-part",
+    // faces: "faces",
+    // axes: "axes",
+    ref_adjacent: "ref-adjacent",
+    ref_child: "ref-child",
+    ref_parent: "ref-parent",
+    // default_inserted_modulescript: "default-inserted-modulescript",
+}
