e8eed813115ce18133a94f4db1bcf6a094bbe8c6
Kampfkarren <boynedmaster@gmail.com>
2021-03-04 23:44:16 -0800

    get_attributes

--- rbx_types/src/lib.rs
+++ rbx_types/src/lib.rs
@@ -5,6 +5,7 @@ mod serde_util;
 #[cfg(feature = "legacy-compat")]
 mod legacy_compat;
 
+mod attributes;
 mod axes;
 mod basic_types;
 mod binary_string;
@@ -17,6 +18,7 @@ mod referent;
 mod shared_string;
 mod variant;
 
+pub use attributes::*;
 pub use axes::*;
 pub use basic_types::*;
 pub use binary_string::*;
