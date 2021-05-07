d218f97398f899543f6c4b75bd23bb8d6b080906
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2021-05-07 05:56:29 -0700

    Add Instance.Name to blacklist

--- generate_reflection/plugin/main.lua
+++ generate_reflection/plugin/main.lua
@@ -9,6 +9,7 @@ local propertyBlacklist = {
 	RequestQueueSize = true, -- on ContentProvider, mistakenly marked serializable
 
 	-- Stuff that doesn't have meaningful defaults
+	Name = true,
 	ClassName = true,
 	Archivable = true,
 	Parent = true,
