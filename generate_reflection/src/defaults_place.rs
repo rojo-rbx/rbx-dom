15d6f4985e032ff07f88591669fc809f092ca32a
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2021-06-08 17:55:23 -0700

    Log the error

--- generate_reflection/src/defaults_place.rs
+++ generate_reflection/src/defaults_place.rs
@@ -235,9 +235,13 @@ fn roundtrip_place_through_studio(place_contents: &str) -> anyhow::Result<Studio
 
         match did_send_chord {
             Ok(()) => (),
-            Err(_) => println!(
+            Err(err) => {
+                log::error!("{}", err);
+
+                println!(
                 "Failed to send key chord to Roblox Studio. Please save the opened place manually."
-            ),
+                )
+            }
         }
     }
 
