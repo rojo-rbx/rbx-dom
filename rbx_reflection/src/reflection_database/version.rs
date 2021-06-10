8708f4bcb4810ec7dccd7a1f1f0a16de1a941a6c
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2021-06-09 21:22:12 -0700

    Update database to version 0.482.0.424268

--- rbx_reflection/src/reflection_database/version.rs
+++ rbx_reflection/src/reflection_database/version.rs
@@ -1,6 +1,6 @@
 // This file is automatically @generated.
 #![allow(clippy::unreadable_literal)]
 pub const RBX_VERSION_MAJOR: u32 = 0;
-pub const RBX_VERSION_MINOR: u32 = 460;
+pub const RBX_VERSION_MINOR: u32 = 482;
 pub const RBX_VERSION_PATCH: u32 = 0;
-pub const RBX_VERSION_BUILD: u32 = 416222;
+pub const RBX_VERSION_BUILD: u32 = 424268;
