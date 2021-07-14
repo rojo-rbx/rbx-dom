8a922cee2a9c4aa30f67cd96e798177161077f7d
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2021-07-14 00:42:49 -0700

    Also match Color3uint8

--- rbx_binary/src/deserializer/state.rs
+++ rbx_binary/src/deserializer/state.rs
@@ -939,7 +939,7 @@ impl<'a, R: Read> DeserializerState<'a, R> {
                 }
             },
             Type::Color3uint8 => match canonical_type {
-                VariantType::Color3 => {
+                VariantType::Color3 | VariantType::Color3uint8 => {
                     let len = type_info.referents.len();
                     let mut r = vec![0; len];
                     let mut g = vec![0; len];
