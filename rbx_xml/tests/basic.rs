f9b3775b5dbff702eaa0d432e707b7a19227e8dd
Lucien Greathouse <me@lpghatguy.com>
2022-04-24 14:47:43 -0400

    rbx_xml: Promote 'basic' test, add Tags serialization test

--- rbx_xml/tests/basic.rs
+++ rbx_xml/tests/basic.rs
@@ -1,6 +1,7 @@
 //! Temporary tests while re-bootstrapping rbx_xml
 
-use rbx_dom_weak::types::Variant;
+use rbx_dom_weak::types::{Tags, Variant};
+use rbx_dom_weak::{InstanceBuilder, WeakDom};
 
 #[test]
 fn with_bool() {
@@ -24,4 +25,16 @@ fn with_bool() {
     assert_eq!(child.name, "BoolValue");
     assert_eq!(child.class, "BoolValue");
     assert_eq!(child.properties.get("Value"), Some(&Variant::Bool(true)));
+}
+
+#[test]
+fn empty_tags() {
+    let _ = env_logger::try_init();
+
+    let part = InstanceBuilder::new("Part").with_property("Tags", Tags::new());
+    let dom = WeakDom::new(part);
+
+    let mut encoded = Vec::new();
+    rbx_xml::to_writer_default(&mut encoded, &dom, &[dom.root_ref()]).unwrap();
+    insta::assert_snapshot!(std::str::from_utf8(&encoded).unwrap());
 }
