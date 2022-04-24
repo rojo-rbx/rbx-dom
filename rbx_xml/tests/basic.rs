2383cc2bacc2fd04179976e51153a9b4d59f41fa
Lucien Greathouse <me@lpghatguy.com>
2022-04-24 14:57:03 -0400

    Throw together more tests for Tags serialization in rbx_xml

--- rbx_xml/tests/basic.rs
+++ rbx_xml/tests/basic.rs
@@ -28,10 +28,50 @@ fn with_bool() {
 }
 
 #[test]
-fn empty_tags() {
+fn read_tags() {
+    let _ = env_logger::try_init();
+
+    let document = r#"
+        <roblox version="4">
+            <Item class="Folder" referent="hello">
+                <Properties>
+                    <BinaryString name="Tags">SGVsbG8AV29ybGQ=</BinaryString>
+                </Properties>
+            </Item>
+        </roblox>
+    "#;
+
+    let dom = rbx_xml::from_str_default(document).unwrap();
+    let folder = dom.get_by_ref(dom.root().children()[0]).unwrap();
+
+    let mut tags = Tags::new();
+    tags.push("Hello");
+    tags.push("World");
+
+    assert_eq!(folder.properties.get("Tags"), Some(&Variant::Tags(tags)));
+}
+
+#[test]
+fn write_empty_tags() {
     let _ = env_logger::try_init();
 
     let part = InstanceBuilder::new("Part").with_property("Tags", Tags::new());
+    let dom = WeakDom::new(part);
+
+    let mut encoded = Vec::new();
+    rbx_xml::to_writer_default(&mut encoded, &dom, &[dom.root_ref()]).unwrap();
+    insta::assert_snapshot!(std::str::from_utf8(&encoded).unwrap());
+}
+
+#[test]
+fn write_tags() {
+    let _ = env_logger::try_init();
+
+    let mut tags = Tags::new();
+    tags.push("Hello");
+    tags.push("World");
+
+    let part = InstanceBuilder::new("Part").with_property("Tags", tags);
     let dom = WeakDom::new(part);
 
     let mut encoded = Vec::new();
