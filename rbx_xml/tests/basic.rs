b2585a28c1a08b5a3317113e525d11c9c7890074
Lucien Greathouse <me@lpghatguy.com>
2022-06-10 18:57:56 -0400

    Add attributes test and update snapshots

--- rbx_xml/tests/basic.rs
+++ rbx_xml/tests/basic.rs
@@ -1,6 +1,9 @@
 //! Temporary tests while re-bootstrapping rbx_xml
 
-use rbx_dom_weak::types::{Tags, Variant};
+use rbx_dom_weak::types::{
+    Attributes, BrickColor, Color3, ColorSequence, ColorSequenceKeypoint, NumberRange,
+    NumberSequence, NumberSequenceKeypoint, Tags, Variant,
+};
 use rbx_dom_weak::{InstanceBuilder, WeakDom};
 
 #[test]
@@ -77,4 +80,103 @@ fn write_tags() {
     let mut encoded = Vec::new();
     rbx_xml::to_writer_default(&mut encoded, &dom, &[dom.root_ref()]).unwrap();
     insta::assert_snapshot!(std::str::from_utf8(&encoded).unwrap());
+}
+
+#[test]
+fn read_attributes() {
+    let _ = env_logger::try_init();
+
+    let document = r#"
+        <roblox version="4">
+            <Item class="Folder" referent="RBX10E3276249364E44B1EBE3BF36E14C1D">
+                <Properties>
+                    <BinaryString name="AttributesSerialize"><![CDATA[DwAAAAMAAABOYU4GAAAAAAAA+P8IAAAASW5maW5pdHkGAAAAAAAA8H8NAAAAQ29sb3JTZXF1
+        ZW5jZRkDAAAAAAAAAAAAAAAAAIA/AAAAAAAAAAAAAAAAAAAAPwAAAAAAAIA/AAAAAAAAAAAA
+        AIA/AAAAAAAAAAAAAIA/BwAAAFZlY3RvcjMRAACAPwAAAEAAAEBABwAAAFZlY3RvcjIQAAAg
+        QQAASEIOAAAATnVtYmVyU2VxdWVuY2UXAwAAAAAAAAAAAAAAAACAPwAAAAAAAAA/AAAAAAAA
+        AAAAAIA/AACAPwYAAABDb2xvcjMPo6IiPwAAAAAAAIA/CgAAAEJyaWNrQ29sb3IO7AMAAAQA
+        AABSZWN0HAAAgD8AAABAAABAQAAAgEAFAAAAVURpbTIKAAAAPwoAAAAzMzM/HgAAAAQAAABV
+        RGltCQAAAD9kAAAACwAAAE51bWJlclJhbmdlGwAAoEAAACBBBgAAAE51bWJlcgYAAAAAgBzI
+        QAcAAABCb29sZWFuAwEGAAAAU3RyaW5nAg0AAABIZWxsbywgd29ybGQh]]></BinaryString>
+                    <string name="Name">Folder</string>
+                </Properties>
+            </Item>
+        </roblox>
+    "#;
+
+    let dom = rbx_xml::from_str_default(document).unwrap();
+    let folder = dom.get_by_ref(dom.root().children()[0]).unwrap();
+
+    assert_eq!(folder.properties.get("AttributesSerialize"), None);
+    let folder_attributes = match folder.properties.get("Attributes") {
+        Some(Variant::Attributes(attrs)) => attrs,
+        Some(other) => panic!(
+            "Attributes property was not Attributes, it was: {:?}",
+            other.ty()
+        ),
+        None => panic!("Attributes property was missing"),
+    };
+
+    let mut attributes = Attributes::new();
+    attributes.insert("Boolean".into(), true.into());
+    attributes.insert("BrickColor".into(), BrickColor::ReallyRed.into());
+    attributes.insert("Color3".into(), Color3::new(162.0 / 255.0, 0.0, 1.0).into());
+    attributes.insert(
+        "ColorSequence".into(),
+        ColorSequence {
+            keypoints: vec![
+                ColorSequenceKeypoint {
+                    time: 0.0,
+                    color: Color3::new(1.0, 0.0, 0.0),
+                },
+                ColorSequenceKeypoint {
+                    time: 0.5,
+                    color: Color3::new(0.0, 1.0, 0.0),
+                },
+                ColorSequenceKeypoint {
+                    time: 1.0,
+                    color: Color3::new(0.0, 0.0, 1.0),
+                },
+            ],
+        }
+        .into(),
+    );
+    attributes.insert("Number".into(), 12345.0f64.into());
+    attributes.insert("NumberRange".into(), NumberRange::new(5.0, 10.0).into());
+    attributes.insert(
+        "NumberSequence".into(),
+        NumberSequence {
+            keypoints: vec![
+                NumberSequenceKeypoint {
+                    time: 0.0,
+                    value: 1.0,
+                    envelope: 0.0,
+                },
+                NumberSequenceKeypoint {
+                    time: 0.5,
+                    value: 0.0,
+                    envelope: 0.0,
+                },
+                NumberSequenceKeypoint {
+                    time: 1.0,
+                    value: 1.0,
+                    envelope: 0.0,
+                },
+            ],
+        }
+        .into(),
+    );
+    // attributes.insert("Rect".into())
+    // - Rect: `1, 2, 3, 4`
+    // - String: `Hello, world!`
+    // - UDim: `0.5, 100`
+    // - UDim2: `{0.5, 10}, {0.7, 30}`
+    // - Vector2: `10, 50`
+    // - Vector3: `1, 2, 3`
+    // - Infinity: `math.huge`
+    // - NaN: `0/0`
+
+    for (key, value) in attributes {
+        assert_eq!(folder_attributes.get(key), Some(&value));
+    }
 }
