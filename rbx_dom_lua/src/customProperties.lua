2a25fd61c4023319960eb77616b07d440abfa701
Max <clonet1019@gmail.com>
2022-07-05 14:49:47 -0500

    Add support for writing to MeshPart.MeshId

--- rbx_dom_lua/src/customProperties.lua
+++ rbx_dom_lua/src/customProperties.lua
@@ -1,8 +1,10 @@
-local CollectionService = game:GetService("CollectionService")
-
 -- Defines how to read and write properties that aren't directly scriptable.
---
 -- The reflection database refers to these as having scriptability = "Custom"
+
+local CollectionService = game:GetService("CollectionService")
+local InsertService = game:GetService("InsertService")
+local MESH_DISPATCH: { [MeshPart]: thread } = {}
+
 return {
 	Instance = {
 		Attributes = {
@@ -61,4 +63,78 @@ return {
 			end,
 		},
 	},
+	MeshPart = {
+		MeshID = {
+			read = function(instance)
+				return true, instance.MeshId
+			end,
+			write = function(meshPart: MeshPart, _, meshId: string)
+				if meshPart.MeshId == meshId then
+					return
+				end
+
+				local dispatch: thread = MESH_DISPATCH[meshPart]
+
+				if dispatch then
+					task.cancel(dispatch)
+				end
+
+				dispatch = task.defer(function()
+					local applied = false
+					local lastErr: string
+					local retries = 0
+
+					while not applied do
+						local success: boolean, response: MeshPart | string = pcall(function()
+							return InsertService:CreateMeshPartAsync(meshId, meshPart.CollisionFidelity, meshPart.RenderFidelity)
+						end)
+
+						if success and typeof(response) == "Instance" then
+							-- If the properties of what we're applying differ from what the MeshPart
+							-- currently wants, try again without increasing the retry counter.
+
+							if response.CollisionFidelity ~= meshPart.CollisionFidelity then
+								continue
+							end
+
+							if response.RenderFidelity ~= meshPart.RenderFidelity then
+								continue
+							end
+
+							meshPart:ApplyMesh(response)
+							applied = true
+
+							break
+						elseif typeof(response) == "string" then
+							lastErr = response
+
+							if lastErr:find("invalid mesh asset") then
+								break
+							end
+
+							retries += 1
+
+							if retries < 5 then
+								task.wait(retries / 2)
+							else
+								break
+							end
+						end
+
+						error("Unexpected response from CreateMeshPartAsync!", 2)
+					end
+
+					if not applied then
+						warn("Error creating MeshPart:", lastErr)
+					end
+
+					if MESH_DISPATCH[meshPart] == dispatch then
+						MESH_DISPATCH[meshPart] = nil
+					end
+				end)
+
+				MESH_DISPATCH[meshPart] = dispatch
+			end,
+		},
+	}
 }
