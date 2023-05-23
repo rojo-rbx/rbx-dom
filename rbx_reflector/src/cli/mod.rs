e757264f9d1f11c78ecb205cd5406274dab4aa3a
nezuo <hinckleytmicah@gmail.com>
2023-05-23 16:44:42 -0700

    Add values command

--- rbx_reflector/src/cli/mod.rs
+++ rbx_reflector/src/cli/mod.rs
@@ -1,11 +1,13 @@
 mod defaults_place;
 mod dump;
 mod generate;
+mod values;
 
 use clap::Parser;
 
 use self::{
     defaults_place::DefaultsPlaceSubcommand, dump::DumpSubcommand, generate::GenerateSubcommand,
+    values::ValuesSubcommand,
 };
 
 #[derive(Debug, Parser)]
@@ -20,6 +22,7 @@ pub enum Subcommand {
     Dump(DumpSubcommand),
     DefaultsPlace(DefaultsPlaceSubcommand),
     Generate(GenerateSubcommand),
+    Values(ValuesSubcommand),
 }
 
 impl Args {
@@ -28,6 +31,7 @@ impl Args {
             Subcommand::Dump(sub) => sub.run(),
             Subcommand::DefaultsPlace(sub) => sub.run(),
             Subcommand::Generate(sub) => sub.run(),
+            Subcommand::Values(sub) => sub.run(),
         }
     }
 }
