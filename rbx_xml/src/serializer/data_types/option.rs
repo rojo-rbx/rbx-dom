ac0848cc08f02e68a5c32f8beb6f83901e32a243
Dekkonot <git@dekkonot.com>
2023-06-30 11:45:05 -0700

    Add tests for CFrame + Option<CFrame> serializer

--- rbx_xml/src/serializer/data_types/option.rs
+++ rbx_xml/src/serializer/data_types/option.rs
@@ -16,3 +16,44 @@ pub fn optional_cframe_serializer<W: io::Write>(
     }
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::serialize_test;
+    use rbx_dom_weak::types::{Matrix3, Vector3};
+
+    #[test]
+    fn optional_cframe_none() {
+        serialize_test!(optional_cframe_serializer, None::<CFrame>, "");
+    }
+
+    #[test]
+    fn optional_cframe_some() {
+        serialize_test!(
+            optional_cframe_serializer,
+            Some(CFrame::new(
+                Vector3::new(10.0, 20.0, 30.0),
+                Matrix3 {
+                    x: Vector3::new(f32::INFINITY, f32::NEG_INFINITY, 0.0),
+                    y: Vector3::new(0.5, 1.5, 1.0),
+                    z: Vector3::new(-0.5, -10.0, 0.15625)
+                }
+            )),
+            r#"<CFrame>
+  <X>10</X>
+  <Y>20</Y>
+  <Z>30</Z>
+  <R00>INF</R00>
+  <R01>-INF</R01>
+  <R02>0</R02>
+  <R10>0.5</R10>
+  <R11>1.5</R11>
+  <R12>1</R12>
+  <R20>-0.5</R20>
+  <R21>-10</R21>
+  <R22>0.15625</R22>
+</CFrame>"#
+        )
+    }
+}
