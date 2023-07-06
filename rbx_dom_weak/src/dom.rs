7b944561207534cf22d9abcb74b0f7fe91ad5240
Kenneth Loeffler <kenloef@gmail.com>
2023-07-05 23:13:11 -0700

    ref_into_instancebuiler->clone_ref_as_builder, add a little docs

--- rbx_dom_weak/src/dom.rs
+++ rbx_dom_weak/src/dom.rs
@@ -249,11 +249,11 @@ impl WeakDom {
     /// rewritten to point to the cloned instances.
     pub fn clone_within(&mut self, referent: Ref) -> Ref {
         let mut ctx = CloneContext::default();
-        let root_builder = self.ref_into_instancebuilder(&mut ctx, referent);
+        let root_builder = self.clone_ref_as_builder(&mut ctx, referent);
         let root_ref = self.insert(Ref::none(), root_builder);
 
         while let Some((cloned_parent, uncloned_child)) = ctx.queue.pop_front() {
-            let builder = self.ref_into_instancebuilder(&mut ctx, uncloned_child);
+            let builder = self.clone_ref_as_builder(&mut ctx, uncloned_child);
             self.insert(cloned_parent, builder);
         }
 
@@ -270,11 +270,11 @@ impl WeakDom {
     /// rewritten to point to the cloned instances.
     pub fn clone_into_external(&self, referent: Ref, dest: &mut WeakDom) -> Ref {
         let mut ctx = CloneContext::default();
-        let root_builder = self.ref_into_instancebuilder(&mut ctx, referent);
+        let root_builder = self.clone_ref_as_builder(&mut ctx, referent);
         let root_ref = dest.insert(Ref::none(), root_builder);
 
         while let Some((cloned_parent, uncloned_child)) = ctx.queue.pop_front() {
-            let builder = self.ref_into_instancebuilder(&mut ctx, uncloned_child);
+            let builder = self.clone_ref_as_builder(&mut ctx, uncloned_child);
             dest.insert(cloned_parent, builder);
         }
 
@@ -326,11 +326,12 @@ impl WeakDom {
         instance
     }
 
-    fn ref_into_instancebuilder(
-        &self,
-        ctx: &mut CloneContext,
-        original_ref: Ref,
-    ) -> InstanceBuilder {
+    /// Clones the instance with the given referent and context into a new
+    /// InstanceBuilder.
+    ///
+    /// This method only clones the instance's class name, name, and properties; it
+    /// does not clone any children.
+    fn clone_ref_as_builder(&self, ctx: &mut CloneContext, original_ref: Ref) -> InstanceBuilder {
         let instance = self
             .get_by_ref(original_ref)
             .expect("Cannot clone an instance that does not exist");
