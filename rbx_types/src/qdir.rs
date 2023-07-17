e1b05cd108d4fbd560d1b28393f84df92bd915b6
Kenneth Loeffler <kenloef@gmail.com>
2023-07-17 14:05:07 -0700

    Add QDir to rbx_types

---
+++ rbx_types/src/qdir.rs
@@ -0,0 +1,43 @@
+use std::path::{Path, PathBuf};
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(transparent)
+)]
+pub struct QDir {
+    path_buf: PathBuf,
+}
+
+impl<'a> QDir {
+    pub fn new() -> Self {
+        QDir {
+            path_buf: PathBuf::new(),
+        }
+    }
+
+    pub fn path_buf(&'a self) -> &'a PathBuf {
+        &self.path_buf
+    }
+}
+
+impl AsRef<Path> for QDir {
+    fn as_ref(&self) -> &Path {
+        self.path_buf.as_path()
+    }
+}
+
+impl From<PathBuf> for QDir {
+    fn from(path: PathBuf) -> Self {
+        Self { path_buf: path }
+    }
+}
+
+impl From<&Path> for QDir {
+    fn from(path: &Path) -> Self {
+        Self {
+            path_buf: PathBuf::from(path),
+        }
+    }
+}
