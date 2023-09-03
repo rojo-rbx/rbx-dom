48d8ffb0deec8fa47086d4e5c02bf1c3b39642e1
Dekkonot <git@dekkonot.com>
2023-09-02 21:38:18 -0700

    Use rbx_types error type for rbx_xml

--- rbx_xml/src/types/unique_id.rs
+++ rbx_xml/src/types/unique_id.rs
@@ -1,6 +1,6 @@
 use std::io::{Read, Write};
 
-use rbx_dom_weak::types::{UniqueId, UniqueIdError};
+use rbx_dom_weak::types::UniqueId;
 
 use crate::{
     core::XmlType,
@@ -15,19 +15,12 @@ impl XmlType for UniqueId {
     fn read_xml<R: Read>(reader: &mut XmlEventReader<R>) -> Result<Self, DecodeError> {
         let content = reader.read_characters()?;
 
-        content.parse().map_err(|e| reader.error(e))
+        content
+            .parse()
+            .map_err(|e| reader.error(DecodeErrorKind::TypeError(e)))
     }
 
     fn write_xml<W: Write>(&self, writer: &mut XmlEventWriter<W>) -> Result<(), EncodeError> {
         writer.write_value(&self.to_string())
-    }
-}
-
-impl From<UniqueIdError> for DecodeErrorKind {
-    fn from(value: UniqueIdError) -> Self {
-        DecodeErrorKind::InvalidPropertyData {
-            property_type: "UniqueId",
-            error: value.to_string(),
-        }
     }
 }
