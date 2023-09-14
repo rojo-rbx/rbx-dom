3f5aa3eadbe1183a55b88ce3802ff2b8784536b9
Kenneth Loeffler <kenloef@gmail.com>
2023-09-14 13:11:40 -0700

    Implement SecurityCapabilities in rbx_types

---
+++ rbx_types/src/security_capabilities.rs
@@ -0,0 +1,23 @@
+use std::fmt;
+
+#[derive(Clone, Copy, Default, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(transparent)
+)]
+pub struct SecurityCapabilities {
+    value: u64,
+}
+
+impl SecurityCapabilities {
+    pub fn new(value: u64) -> Self {
+        SecurityCapabilities { value }
+    }
+}
+
+impl fmt::Display for SecurityCapabilities {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.value.fmt(f)
+    }
+}
--- rbx_types/src/variant.rs
+++ rbx_types/src/variant.rs
@@ -1,8 +1,8 @@
 use crate::{
     Attributes, Axes, BinaryString, BrickColor, CFrame, Color3, Color3uint8, ColorSequence,
     Content, Enum, Faces, Font, MaterialColors, NumberRange, NumberSequence, PhysicalProperties,
-    Ray, Rect, Ref, Region3, Region3int16, SharedString, Tags, UDim, UDim2, UniqueId, Vector2,
-    Vector2int16, Vector3, Vector3int16,
+    Ray, Rect, Ref, Region3, Region3int16, SecurityCapabilities, SharedString, Tags, UDim, UDim2,
+    UniqueId, Vector2, Vector2int16, Vector3, Vector3int16,
 };
 
 /// Reduces boilerplate from listing different values of Variant by wrapping
@@ -132,6 +132,7 @@ make_variant! {
     Font(Font),
     UniqueId(UniqueId),
     MaterialColors(MaterialColors),
+    SecurityCapabilities(SecurityCapabilities),
 }
 
 impl From<&'_ str> for Variant {
