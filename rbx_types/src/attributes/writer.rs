bc70e52138b0be037a653d6a4453551f7d9b9ef4
Micah <micah@uplift.games>
2023-10-23 07:54:05 -0700

    Fix clippy lint

--- rbx_types/src/attributes/writer.rs
+++ rbx_types/src/attributes/writer.rs
@@ -23,8 +23,6 @@ pub(crate) fn write_attributes<W: Write>(
     writer.write_all(&(map.len() as u32).to_le_bytes())?;
 
     for (name, variant) in map {
-        let variant = variant;
-
         write_string(&mut writer, name)?;
 
         let type_id = type_id::from_variant_type(variant.ty())
