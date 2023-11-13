ca2cbf00105b5e848d6db95429f6abbc54b98c42
Micah <micah@uplift.games>
2023-11-13 13:06:06 -0800

    Update README and module documentation

--- rbx_reflection_database/src/lib.rs
+++ rbx_reflection_database/src/lib.rs
@@ -1,3 +1,34 @@
+//! Contains an API to get a Roblox reflection database using the types
+//! from [`rbx_reflection`]. This crate embeds a database for this purpose,
+//! but also provides an API for dependents to get a reflection database
+//! from a consistent location.
+//!
+//! The general way this crate should be used is via [`get`]. This method will
+//! search for a locally stored reflection database and return it if it's
+//! found. If it isn't, it will instead return the bundled one.
+//!
+//! Additionally, this crate exposes [`get_local`] and [`get_bundled`] for
+//! easily only loading the locally stored database or only the bundled one
+//! respectively.
+//!
+//! ## Local Details
+//!
+//! This crate will load a reflection database from the file system if one
+//! exists in the default location. This location varies upon the OS and is
+//! specified here:
+//!
+//! | OS      | Location                                                            |
+//! |:--------|:--------------------------------------------------------------------|
+//! | Windows | `%localappdata%/.rbxreflection/database.msgpack`                    |
+//! | MacOS   | `$HOME/Library/Application Support/.rbxreflection/database.msgpack` |
+//! | Linux   | `$HOME/.rbxreflection/database.msgpack`                             |
+//!
+//! Additionally, a location override may be specified via the `RBX_DATABASE`
+//! environmental variable.
+//!
+//! Both the default `database.msgpack` files and any files pointed to by
+//! `RBX_DATABASE` must be valid MessagePack serializations of a
+//! [`ReflectionDatabase`] if they're present.
 use rbx_reflection::ReflectionDatabase;
 
 use std::{env, fs, path::PathBuf};
