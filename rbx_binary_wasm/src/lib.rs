51c36b9f08c006b334e510de4429c272062b9df9
Martin Kagermeier <martin@uplift.games>
2024-10-04 22:01:39 -0500

    Cleanup remnants of template

--- rbx_binary_wasm/src/lib.rs
+++ rbx_binary_wasm/src/lib.rs
@@ -1,19 +1,12 @@
 mod utils;
 
+use utils::set_panic_hook;
 use wasm_bindgen::prelude::*;
-
-// #[wasm_bindgen]
-// extern "C" {
-//     fn alert(s: &str);
-// }
-
-// #[wasm_bindgen]
-// pub fn greet() {
-//     alert("Hello, rbx_binary_wasm!");
-// }
 
 #[wasm_bindgen]
 pub fn decode_file(binary_file: Vec<u8>) -> JsValue {
+    set_panic_hook();
+
     let dom = rbx_binary::from_reader(&binary_file[..]);
 
     match dom {
