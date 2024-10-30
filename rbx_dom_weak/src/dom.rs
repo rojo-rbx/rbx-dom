a935a02593f345e5a3ddbab06a8fb247130d9e99
kennethloeffler <kenloef@gmail.com>
2024-10-30 19:40:26 +0000

    Reserve space for UniqueIds, too

--- rbx_dom_weak/src/dom.rs
+++ rbx_dom_weak/src/dom.rs
@@ -36,6 +36,7 @@ impl WeakDom {
     /// the WeakDom.
     pub fn reserve(&mut self, additional: usize) {
         self.instances.reserve(additional);
+        self.unique_ids.reserve(additional);
     }
 
     /// Consumes the WeakDom, returning its underlying root ref and backing
