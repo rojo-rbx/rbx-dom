e5bb471dc8bb3f011bd7188e4ff53a969106cb0d
kennethloeffler <kenloef@gmail.com>
2024-10-31 11:17:03 +0000

    Fix clippy lints

--- rbx_binary/benches/suite/util.rs
+++ rbx_binary/benches/suite/util.rs
@@ -14,14 +14,14 @@ fn serialize_bench<T: Measurement>(group: &mut BenchmarkGroup<T>, tree: &WeakDom
     let root_ref = tree.root_ref();
     let mut buffer = Vec::new();
 
-    rbx_binary::to_writer(&mut buffer, &tree, &[root_ref]).unwrap();
+    rbx_binary::to_writer(&mut buffer, tree, &[root_ref]).unwrap();
     let buffer_len = buffer.len();
 
     group.bench_function("Serialize", |b| {
         b.iter_batched(
             || Vec::with_capacity(buffer_len),
             |mut buffer: Vec<u8>| {
-                rbx_binary::to_writer(&mut buffer, &tree, &[root_ref]).unwrap();
+                rbx_binary::to_writer(&mut buffer, tree, &[root_ref]).unwrap();
             },
             BatchSize::SmallInput,
         )
