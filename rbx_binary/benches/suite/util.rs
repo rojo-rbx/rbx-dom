482c27bc8ded5c658134ace5dbfb205331554289
Kenneth Loeffler <kenloef@gmail.com>
2024-10-31 15:35:03 +0000

    Upgrade criterion to 0.5.1, reorganize benchmarks (#468)

---
+++ rbx_binary/benches/suite/util.rs
@@ -0,0 +1,37 @@
+use criterion::{measurement::Measurement, BatchSize, BenchmarkGroup, Throughput};
+
+pub(crate) fn bench<T: Measurement>(group: &mut BenchmarkGroup<T>, bench_file: &'static [u8]) {
+    serialize_bench(group, bench_file);
+    deserialize_bench(group, bench_file);
+}
+
+fn serialize_bench<T: Measurement>(group: &mut BenchmarkGroup<T>, buffer: &[u8]) {
+    let tree = rbx_binary::from_reader(buffer).unwrap();
+    let root_ref = tree.root_ref();
+    let mut buffer = Vec::new();
+
+    rbx_binary::to_writer(&mut buffer, &tree, &[root_ref]).unwrap();
+    let buffer_len = buffer.len();
+
+    group
+        .throughput(Throughput::Bytes(buffer_len as u64))
+        .bench_function("Serialize", |b| {
+            b.iter_batched(
+                || Vec::with_capacity(buffer_len),
+                |mut buffer: Vec<u8>| {
+                    rbx_binary::to_writer(&mut buffer, &tree, &[root_ref]).unwrap();
+                },
+                BatchSize::SmallInput,
+            )
+        });
+}
+
+fn deserialize_bench<T: Measurement>(group: &mut BenchmarkGroup<T>, buffer: &[u8]) {
+    group
+        .throughput(Throughput::Bytes(buffer.len() as u64))
+        .bench_function("Deserialize", |bencher| {
+            bencher.iter(|| {
+                rbx_binary::from_reader(buffer).unwrap();
+            });
+        });
+}
