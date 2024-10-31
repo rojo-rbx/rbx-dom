1261e9b4c3744082e2ae1c496158a7bba16b3920
kennethloeffler <kenloef@gmail.com>
2024-10-31 15:07:47 +0000

    Move throughput calls to utils
    Serialization would not be accurate before, because the serialized
    size will not necessarily the same as the input file

--- rbx_binary/benches/suite/main.rs
+++ rbx_binary/benches/suite/main.rs
@@ -1,42 +1,34 @@
 mod util;
 
 use crate::util::bench;
-use criterion::{criterion_group, criterion_main, Criterion, SamplingMode, Throughput};
+use criterion::{criterion_group, criterion_main, Criterion, SamplingMode};
 
 pub fn folders_100(c: &mut Criterion) {
-    let bytes = include_bytes!("../files/folders-100.rbxm");
     bench(
-        c.benchmark_group("100 Folders")
-            .throughput(Throughput::Bytes(bytes.len() as u64)),
-        bytes,
+        &mut c.benchmark_group("100 Folders"),
+        include_bytes!("../files/folders-100.rbxm"),
     )
 }
 
 pub fn deep_folders_100(c: &mut Criterion) {
-    let bytes = include_bytes!("../files/deep-folders-100.rbxm");
     bench(
-        c.benchmark_group("100 Deep Folders")
-            .throughput(Throughput::Bytes(bytes.len() as u64)),
-        bytes,
+        &mut c.benchmark_group("100 Deep Folders"),
+        include_bytes!("../files/deep-folders-100.rbxm"),
     )
 }
 
 pub fn modulescripts_100_lines_100(c: &mut Criterion) {
-    let bytes = include_bytes!("../files/modulescripts-100-lines-100.rbxm");
     bench(
-        c.benchmark_group("100 100-line ModuleScripts")
-            .throughput(Throughput::Bytes(bytes.len() as u64)),
-        bytes,
+        &mut c.benchmark_group("100 100-line ModuleScripts"),
+        include_bytes!("../files/modulescripts-100-lines-100.rbxm"),
     )
 }
 
 pub fn parts_1000(c: &mut Criterion) {
-    let bytes = include_bytes!("../files/parts-1000.rbxm");
     bench(
         c.benchmark_group("1,000 Parts")
-            .sampling_mode(SamplingMode::Flat)
-            .throughput(Throughput::Bytes(bytes.len() as u64)),
-        bytes,
+            .sampling_mode(SamplingMode::Flat),
+        include_bytes!("../files/parts-1000.rbxm"),
     )
 }
 
