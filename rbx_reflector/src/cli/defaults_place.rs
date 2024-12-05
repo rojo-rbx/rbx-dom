d4e011988ba1d7bdc062480c305a5c1468e151ba
Micah <micah@uplift.games>
2024-12-05 10:51:01 -0800

    Add exceptions in rbx_reflector for settings singletons

--- rbx_reflector/src/cli/defaults_place.rs
+++ rbx_reflector/src/cli/defaults_place.rs
@@ -135,6 +135,11 @@ fn generate_place_with_all_classes(path: &PathBuf, dump: &Dump) -> anyhow::Resul
             | "ScriptDebugger" | "PackageLink" | "Ad" | "AdPortal" | "AdGui"
             | "InternalSyncItem" => continue,
 
+            // Settings singletons cannot be put into a DataModel. This changed
+            // in release 653.
+            "DebugSettings" | "GameSettings" | "LuaSettings" | "NetworkSettings"
+            | "PhysicsSettings" | "RenderSettings" | "Studio" | "TaskScheduler" => continue,
+
             // This class will cause studio to crash on close.
             "VoiceSource" => continue,
 
