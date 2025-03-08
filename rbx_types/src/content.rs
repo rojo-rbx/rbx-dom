6d5125de611ebf115a19d569ebbfe617bc9ca2bb
Micah <micah@uplift.games>
2025-03-07 16:38:21 -0800

    ContentId? In this economy?

--- rbx_types/src/content.rs
+++ rbx_types/src/content.rs
@@ -72,3 +72,66 @@ impl From<&'_ str> for Content {
         }
     }
 }
+/// A reference to a Roblox asset.
+///
+/// When exposed to Lua, this is just a string.
+#[derive(Debug, Clone, Default, PartialEq, Eq, Hash)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(transparent)
+)]
+
+pub struct ContentId {
+    url: String,
+}
+
+impl ContentId {
+    #[inline]
+    pub fn new() -> Self {
+        ContentId { url: String::new() }
+    }
+
+    #[inline]
+    pub fn into_string(self) -> String {
+        self.url
+    }
+}
+
+impl From<String> for ContentId {
+    fn from(url: String) -> Self {
+        Self { url }
+    }
+}
+
+impl From<&'_ str> for ContentId {
+    fn from(url: &str) -> Self {
+        Self {
+            url: url.to_owned(),
+        }
+    }
+}
+
+impl AsRef<str> for ContentId {
+    fn as_ref(&self) -> &str {
+        &self.url
+    }
+}
+
+impl AsRef<String> for ContentId {
+    fn as_ref(&self) -> &String {
+        &self.url
+    }
+}
+
+impl AsMut<str> for ContentId {
+    fn as_mut(&mut self) -> &mut str {
+        &mut self.url
+    }
+}
+
+impl AsMut<String> for ContentId {
+    fn as_mut(&mut self) -> &mut String {
+        &mut self.url
+    }
+}
