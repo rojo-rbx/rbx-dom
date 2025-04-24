42b693bab2d40295d2624fe8be732d4c42ed97d0
LouieK22 <10765258+LouieK22@users.noreply.github.com>
2025-04-23 20:28:27 -0500

    implement lz4_flex

--- rbx_binary/src/chunk.rs
+++ rbx_binary/src/chunk.rs
@@ -41,7 +41,7 @@ impl Chunk {
                 zstd::bulk::decompress(&compressed_data, header.len as usize)?
             } else {
                 log::trace!("LZ4 compression");
-                lz4::block::decompress(&compressed_data, Some(header.len as i32))?
+                lz4_flex::block::decompress(&compressed_data, header.len as usize).unwrap()
             }
         };
 
@@ -83,7 +83,7 @@ impl ChunkBuilder {
 
         match self.compression {
             CompressionType::Lz4 => {
-                let compressed = lz4::block::compress(&self.buffer, None, false)?;
+                let compressed = lz4_flex::block::compress(&self.buffer);
 
                 writer.write_le_u32(compressed.len() as u32)?;
                 writer.write_le_u32(self.buffer.len() as u32)?;
