b572f231d390bf119d1a1fcd904dbc71eca77a26
Quaternions <krakow20@gmail.com>
2025-05-03 12:08:06 -0700

    rbx_dom_weak: directly insert InstanceBuilder.properties into Instance.properties

--- rbx_dom_weak/src/dom.rs
+++ rbx_dom_weak/src/dom.rs
@@ -192,7 +192,7 @@ impl WeakDom {
                     parent,
                     name: builder.name,
                     class: builder.class,
-                    properties: builder.properties.into_iter().collect(),
+                    properties: builder.properties,
                 },
             );
 
