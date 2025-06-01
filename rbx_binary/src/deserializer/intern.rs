378448fddd8661c97f1d7fbcd9db2869eb975a6c
Quaternions <krakow20@gmail.com>
2025-06-01 14:11:27 -0700

    rename StringIntern lifetimes

--- rbx_binary/src/deserializer/intern.rs
+++ rbx_binary/src/deserializer/intern.rs
@@ -1,20 +1,20 @@
-/// A trait representing string internment that lives for at least 'long.
+/// A trait representing string internment that lives for at least 'output.
 /// There is a blanket implementation for any function fn(&str)->&str
 /// so this can be a closure operating your very own string interner.
-pub trait StringIntern<'short, 'long> {
-    /// Take an input string with lifetime 'short and guarantee
-    /// that the identical output string lives for at least 'long.
-    fn intern(&mut self, str: &'short str) -> &'long str;
+pub trait StringIntern<'input, 'output> {
+    /// Take an input string with lifetime 'input and guarantee
+    /// that the identical output string lives for at least 'output.
+    fn intern(&mut self, str: &'input str) -> &'output str;
 }
 
 /// A type alias for a function which implements the StringIntern trait.
-pub type InternFunction<'short, 'long> = fn(&'short str) -> &'long str;
+pub type InternFunction<'input, 'output> = fn(&'input str) -> &'output str;
 
-impl<'short, 'long, F> StringIntern<'short, 'long> for F
+impl<'input, 'output, F> StringIntern<'input, 'output> for F
 where
-    F: FnMut(&'short str) -> &'long str,
+    F: FnMut(&'input str) -> &'output str,
 {
-    fn intern(&mut self, str: &'short str) -> &'long str {
+    fn intern(&mut self, str: &'input str) -> &'output str {
         self(str)
     }
 }
