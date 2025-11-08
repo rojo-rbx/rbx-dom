17e7a610f3d4da4992e0794c7eab2e693d39de66
Rhys Lloyd <krakow20@gmail.com>
2025-11-08 05:11:00 -0800

    fix doc comments

--- rbx_binary/src/core.rs
+++ rbx_binary/src/core.rs
@@ -281,7 +281,7 @@ pub fn read_string<'a>(slice: &mut &'a [u8]) -> io::Result<&'a str> {
 }
 
 /// Create an iterator that reads chunks of N interleaved bytes.
-/// This function allocates `N * len` bytes before reading.
+/// Splits `N * len` bytes from the slice.
 ///
 /// This function is not part of RbxReadExt.
 pub fn read_interleaved_bytes<'a, const N: usize>(
@@ -297,8 +297,7 @@ pub fn read_interleaved_bytes<'a, const N: usize>(
 }
 
 /// Creates an iterator of `len` big-endian i32 values.
-/// The bytes are read into a buffer immediately,
-/// and the values are transformed during iteration.
+/// The values are transformed during iteration.
 ///
 /// This function is not part of RbxReadExt.
 pub fn read_interleaved_i32_array<'a>(
@@ -309,8 +308,7 @@ pub fn read_interleaved_i32_array<'a>(
 }
 
 /// Creates an iterator of `len` big-endian u32 values.
-/// The bytes are read into a buffer immediately,
-/// and the values are transformed during iteration.
+/// The values are transformed during iteration.
 ///
 /// This function is not part of RbxReadExt.
 pub fn read_interleaved_u32_array<'a>(
@@ -321,8 +319,7 @@ pub fn read_interleaved_u32_array<'a>(
 }
 
 /// Creates an iterator of `len` big-endian f32 values.
-/// The bytes are read into a buffer immediately,
-/// and the values are properly unrotated during iteration.
+/// The values are properly unrotated during iteration.
 ///
 /// This function is not part of RbxReadExt.
 pub fn read_interleaved_f32_array<'a>(
@@ -334,8 +331,7 @@ pub fn read_interleaved_f32_array<'a>(
 }
 
 /// Creates an iterator of `len` big-endian i32 values.
-/// The bytes are read into a buffer immediately,
-/// and the values are properly untransformed and accumulated
+/// The values are properly untransformed and accumulated
 /// so as to properly read arrays of referent values.
 ///
 /// This function is not part of RbxReadExt.
@@ -354,8 +350,7 @@ pub fn read_referent_array<'a>(
 }
 
 /// Creates an iterator of `len` big-endian i64 values.
-/// The bytes are read into a buffer immediately,
-/// and the values are transformed during iteration.
+/// The values are transformed during iteration.
 ///
 /// This function is not part of RbxReadExt.
 pub fn read_interleaved_i64_array<'a>(
