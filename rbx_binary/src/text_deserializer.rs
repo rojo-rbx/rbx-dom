85770df7ac464db618c444d5c8e0f186e185b072
Rhys Lloyd <krakow20@gmail.com>
2025-11-15 18:38:46 -0800

    appease clippy

--- rbx_binary/src/text_deserializer.rs
+++ rbx_binary/src/text_deserializer.rs
@@ -589,7 +589,7 @@ impl DecodedValues {
                 let b = chunk.read_slice(prop_count).unwrap();
 
                 let values = r
-                    .into_iter()
+                    .iter()
                     .zip(g)
                     .zip(b)
                     .map(|((r, g), b)| Color3uint8::new(*r, *g, *b))
