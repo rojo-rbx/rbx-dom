17e31e7e8d7298dd7d14514738c1aa3c69d7a5c4
Rhys Lloyd <krakow20@gmail.com>
2026-07-26 16:37:04 -0700

    AttributeWriter: move write_* to methods

--- rbx_types/src/attributes/writer.rs
+++ rbx_types/src/attributes/writer.rs
@@ -22,91 +22,140 @@ impl<W: Write> AttributeWriter<W, false> {
     }
     pub fn write_len(self, len: u32) -> Result<AttributeWriter<W, true>, AttributeError> {
         let mut writer = self.writer;
-        write_u32(&mut writer, len)?;
+        writer.write_all(&len.to_le_bytes())?;
         Ok(AttributeWriter { writer })
     }
 }
 impl<W: Write> AttributeWriter<W, true> {
+    fn write_i32(&mut self, n: i32) -> io::Result<()> {
+        self.writer.write_all(&n.to_le_bytes())
+    }
+
+    fn write_f32(&mut self, n: f32) -> io::Result<()> {
+        self.writer.write_all(&n.to_le_bytes())
+    }
+
+    fn write_f64(&mut self, n: f64) -> io::Result<()> {
+        self.writer.write_all(&n.to_le_bytes())
+    }
+
+    fn write_u32(&mut self, n: u32) -> io::Result<()> {
+        self.writer.write_all(&n.to_le_bytes())
+    }
+
+    fn write_u16(&mut self, n: u16) -> io::Result<()> {
+        self.writer.write_all(&n.to_le_bytes())
+    }
+
+    fn write_u8(&mut self, n: u8) -> io::Result<()> {
+        self.writer.write_all(&n.to_le_bytes())
+    }
+
+    fn write_color3(&mut self, color: Color3) -> io::Result<()> {
+        self.write_f32(color.r)?;
+        self.write_f32(color.g)?;
+        self.write_f32(color.b)
+    }
+
+    fn write_string<T: AsRef<[u8]>>(&mut self, string: T) -> io::Result<()> {
+        let bytes = string.as_ref();
+        self.write_u32(bytes.len() as u32)?;
+        self.writer.write_all(bytes)
+    }
+
+    fn write_udim(&mut self, udim: UDim) -> io::Result<()> {
+        self.write_f32(udim.scale)?;
+        self.writer.write_all(&udim.offset.to_le_bytes())
+    }
+
+    fn write_vector2(&mut self, vector2: Vector2) -> io::Result<()> {
+        self.write_f32(vector2.x)?;
+        self.write_f32(vector2.y)
+    }
+
+    fn write_vector3(&mut self, vector3: Vector3) -> io::Result<()> {
+        self.write_f32(vector3.x)?;
+        self.write_f32(vector3.y)?;
+        self.write_f32(vector3.z)
+    }
+
     pub fn write_attribute(&mut self, name: &str, variant: &Variant) -> Result<(), AttributeError> {
-        write_string(&mut self.writer, name)?;
+        self.write_string(name)?;
 
         let attribute_type = AttributeType::from_variant_type(variant.ty())
             .ok_or_else(|| AttributeError::UnsupportedVariantType(variant.ty()))?;
-        write_u8(&mut self.writer, attribute_type.to_u8())?;
+        self.write_u8(attribute_type.to_u8())?;
 
         match variant {
             Variant::Bool(bool) => self.writer.write_all(&[*bool as u8])?,
-            Variant::BrickColor(color) => write_u32(&mut self.writer, *color as u32)?,
-            Variant::Color3(color) => write_color3(&mut self.writer, *color)?,
+            Variant::BrickColor(color) => self.write_u32(*color as u32)?,
+            Variant::Color3(color) => self.write_color3(*color)?,
             Variant::ColorSequence(sequence) => {
-                write_u32(&mut self.writer, sequence.keypoints.len() as u32)?;
+                self.write_u32(sequence.keypoints.len() as u32)?;
 
                 for keypoint in &sequence.keypoints {
-                    write_f32(&mut self.writer, 0.0)?; // Envelope
-                    write_f32(&mut self.writer, keypoint.time)?;
-                    write_color3(&mut self.writer, keypoint.color)?;
+                    self.write_f32(0.0)?; // Envelope
+                    self.write_f32(keypoint.time)?;
+                    self.write_color3(keypoint.color)?;
                 }
             }
-            Variant::Int32(int) => write_i32(&mut self.writer, *int)?,
-            Variant::Float32(float) => write_f32(&mut self.writer, *float)?,
-            Variant::Float64(float) => write_f64(&mut self.writer, *float)?,
+            Variant::Int32(int) => self.write_i32(*int)?,
+            Variant::Float32(float) => self.write_f32(*float)?,
+            Variant::Float64(float) => self.write_f64(*float)?,
             Variant::NumberRange(range) => {
-                write_f32(&mut self.writer, range.min)?;
-                write_f32(&mut self.writer, range.max)?;
+                self.write_f32(range.min)?;
+                self.write_f32(range.max)?;
             }
             Variant::NumberSequence(sequence) => {
-                write_u32(&mut self.writer, sequence.keypoints.len() as u32)?;
+                self.write_u32(sequence.keypoints.len() as u32)?;
 
                 for keypoint in &sequence.keypoints {
-                    write_f32(&mut self.writer, keypoint.envelope)?;
-                    write_f32(&mut self.writer, keypoint.time)?;
-                    write_f32(&mut self.writer, keypoint.value)?;
+                    self.write_f32(keypoint.envelope)?;
+                    self.write_f32(keypoint.time)?;
+                    self.write_f32(keypoint.value)?;
                 }
             }
             Variant::Rect(rect) => {
-                write_vector2(&mut self.writer, rect.min)?;
-                write_vector2(&mut self.writer, rect.max)?
+                self.write_vector2(rect.min)?;
+                self.write_vector2(rect.max)?
             }
-            Variant::BinaryString(string) => write_string(&mut self.writer, string)?,
-            Variant::String(string) => write_string(&mut self.writer, string)?,
-            Variant::UDim(udim) => write_udim(&mut self.writer, *udim)?,
+            Variant::BinaryString(string) => self.write_string(string)?,
+            Variant::String(string) => self.write_string(string)?,
+            Variant::UDim(udim) => self.write_udim(*udim)?,
             Variant::UDim2(udim2) => {
-                write_udim(&mut self.writer, udim2.x)?;
-                write_udim(&mut self.writer, udim2.y)?
+                self.write_udim(udim2.x)?;
+                self.write_udim(udim2.y)?
             }
-            Variant::Vector2(vector2) => write_vector2(&mut self.writer, *vector2)?,
+            Variant::Vector2(vector2) => self.write_vector2(*vector2)?,
             Variant::Vector3(vector3) => {
-                write_f32(&mut self.writer, vector3.x)?;
-                write_f32(&mut self.writer, vector3.y)?;
-                write_f32(&mut self.writer, vector3.z)?
+                self.write_f32(vector3.x)?;
+                self.write_f32(vector3.y)?;
+                self.write_f32(vector3.z)?
             }
             Variant::CFrame(cframe) => {
-                write_vector3(&mut self.writer, cframe.position)?;
+                self.write_vector3(cframe.position)?;
 
                 let matrix = cframe.orientation;
 
                 if let Some(rotation_id) = matrix.to_basic_rotation_id() {
-                    write_u8(&mut self.writer, rotation_id)?;
+                    self.write_u8(rotation_id)?;
                 } else {
-                    write_u8(&mut self.writer, 0x00)?;
+                    self.write_u8(0x00)?;
 
-                    write_vector3(&mut self.writer, matrix.x)?;
-                    write_vector3(&mut self.writer, matrix.y)?;
-                    write_vector3(&mut self.writer, matrix.z)?;
+                    self.write_vector3(matrix.x)?;
+                    self.write_vector3(matrix.y)?;
+                    self.write_vector3(matrix.z)?;
                 }
             }
             Variant::Font(font) => {
-                write_u16(&mut self.writer, font.weight.as_u16())?;
-                write_u8(&mut self.writer, font.style.as_u8())?;
-                write_string(&mut self.writer, &font.family)?;
-                write_string(
-                    &mut self.writer,
-                    font.cached_face_id.as_deref().unwrap_or_default(),
-                )?;
+                self.write_u16(font.weight.as_u16())?;
+                self.write_u8(font.style.as_u8())?;
+                self.write_string(&font.family)?;
+                self.write_string(font.cached_face_id.as_deref().unwrap_or_default())?;
             }
             Variant::EnumItem(enum_item) => {
-                write_string(&mut self.writer, &enum_item.ty)?;
-                write_u32(&mut self.writer, enum_item.value)?;
+                self.write_string(&enum_item.ty)?;
+                self.write_u32(enum_item.value)?;
             }
 
             other_variant => unreachable!("variant {:?} was not implemented", other_variant),
@@ -114,56 +163,4 @@ impl<W: Write> AttributeWriter<W, true> {
 
         Ok(())
     }
-}
-
-fn write_i32<W: Write>(mut writer: W, n: i32) -> io::Result<()> {
-    writer.write_all(&n.to_le_bytes()[..])
-}
-
-fn write_f32<W: Write>(mut writer: W, n: f32) -> io::Result<()> {
-    writer.write_all(&n.to_le_bytes()[..])
-}
-
-fn write_f64<W: Write>(mut writer: W, n: f64) -> io::Result<()> {
-    writer.write_all(&n.to_le_bytes()[..])
-}
-
-fn write_u32<W: Write>(mut writer: W, n: u32) -> io::Result<()> {
-    writer.write_all(&n.to_le_bytes()[..])
-}
-
-fn write_u16<W: Write>(mut writer: W, n: u16) -> io::Result<()> {
-    writer.write_all(&n.to_le_bytes()[..])
-}
-
-fn write_u8<W: Write>(mut writer: W, n: u8) -> io::Result<()> {
-    writer.write_all(&n.to_le_bytes()[..])
-}
-
-fn write_color3<W: Write>(mut writer: W, color: Color3) -> io::Result<()> {
-    write_f32(&mut writer, color.r)?;
-    write_f32(&mut writer, color.g)?;
-    write_f32(&mut writer, color.b)
-}
-
-fn write_string<T: AsRef<[u8]>, W: Write>(mut writer: W, string: T) -> io::Result<()> {
-    let bytes = string.as_ref();
-    write_u32(&mut writer, bytes.len() as u32)?;
-    writer.write_all(bytes)
-}
-
-fn write_udim<W: Write>(mut writer: W, udim: UDim) -> io::Result<()> {
-    write_f32(&mut writer, udim.scale)?;
-    writer.write_all(&udim.offset.to_le_bytes()[..])
-}
-
-fn write_vector2<W: Write>(mut writer: W, vector2: Vector2) -> io::Result<()> {
-    write_f32(&mut writer, vector2.x)?;
-    write_f32(&mut writer, vector2.y)
-}
-
-fn write_vector3<W: Write>(mut writer: W, vector3: Vector3) -> io::Result<()> {
-    write_f32(&mut writer, vector3.x)?;
-    write_f32(&mut writer, vector3.y)?;
-    write_f32(&mut writer, vector3.z)
 }
